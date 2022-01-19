f73dd7e6b81abe58ac661b9e861bdc31a231e3d0
Bogdan Polygalov <77894393+miamib34ch@users.noreply.github.com>
2022-01-19 21:35:15 +0500

    Create shakerSort.swift

---
+++ shakerSort.swift
@@ -0,0 +1,28 @@
+
+
+func ShakerSort(_ mas: inout[Int]){ //inout - ссылается на объект
+    var left = 0 //левый конец
+    var right = mas.count-1 //правый конец
+    while (left <= right){ //пока не дойдёт до середины
+        for i in (left...right).reversed() where i>left { //двигаемся с права на лево (левого конца, пока больше него) по массиву
+            if (mas[i-1] > mas[i]){
+                mas.swapAt(i-1,i) //меняем местами по индексам
+            }
+        }
+        left+=1
+        for i in left...right where i<right{ //двигаемся с лева на право (правого конца, пока меньше него) по массиву
+            if(mas[i] > mas[i+1]){
+                mas.swapAt(i, i+1)
+            }
+        }
+        right-=1
+    }
+}
+
+var mas = [1,3,2,5]
+ShakerSort(&mas) //& используется с inout
+for i in mas {
+    print(i)
+}
+
+//как пузырьковая, но сначала идёт с левого конца на правый, затем с правого на левый
